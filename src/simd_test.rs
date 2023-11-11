701a352147b19717196bfc78dba11fbac2c29384
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-11-11 15:20:58 -0500

    single module

--- src/simd_test.rs
+++ src/simd_test.rs
@@ -1,88 +1,74 @@
 #[cfg(test)]
-use std::simd::Simd;
-
-#[cfg(test)]
-use std::simd::SimdPartialOrd;
-
-#[cfg(test)]
-use std::simd::ToBitMask;
-
-#[cfg(test)]
-fn find_iter(sorted: &[i32], target: i32) -> usize {
-    return sorted
-        .iter()
-        .position(|&el| target < el)
-        .unwrap_or(sorted.len())
-        + 1;
-}
-
-#[cfg(test)]
-fn find_linear(sorted: &[i32], target: i32) -> usize {
-    for (i, &el) in sorted.iter().enumerate() {
-        if target < el {
-            return i + 1;
-        }
-    }
-    return sorted.len() + 1;
-}
-
-#[cfg(test)]
-fn find_binary(sorted: &[i32], target: i32) -> usize {
-    let mut left = 0;
-    let mut right = sorted.len();
-
-    while left < right {
-        let mid = left + (right - left) / 2;
-
-        if sorted[mid] <= target {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
-    }
-
-    return left + 1;
-}
-
-#[cfg(test)]
-fn find_simd(sorted: &[i32], target: i32) -> usize {
-    let sorted_len = sorted.len();
-
-    let target_8: Simd<i32, 8> = Simd::splat(target);
-
-    let mut i: usize = 0;
-
-    while i < sorted_len {
-        if sorted_len - i < 8 {
-            // this is the last batch: iterate
-            while i < sorted_len {
-                if target < sorted[i] {
-                    return i + 1;
-                }
-                i += 1;
-            }
-        } else {
-            // simd
-            let next_8: Simd<i32, 8> = Simd::from_slice(&sorted[i..(i + 8)]);
-
-            let matches = target_8.simd_lt(next_8);
-            if matches.any() {
-                return i + (matches.to_bitmask().trailing_zeros() as usize) + 1;
-            }
-
-            i += 8;
-        }
-    }
-
-    return sorted_len + 1;
-}
-
-#[cfg(test)]
 mod tests {
-    use crate::simd_test::find_binary;
-    use crate::simd_test::find_iter;
-    use crate::simd_test::find_linear;
-    use crate::simd_test::find_simd;
+    use std::simd::Simd;
+    use std::simd::SimdPartialOrd;
+    use std::simd::ToBitMask;
+
+    fn find_iter(sorted: &[i32], target: i32) -> usize {
+        return sorted
+            .iter()
+            .position(|&el| target < el)
+            .unwrap_or(sorted.len())
+            + 1;
+    }
+
+    fn find_linear(sorted: &[i32], target: i32) -> usize {
+        for (i, &el) in sorted.iter().enumerate() {
+            if target < el {
+                return i + 1;
+            }
+        }
+        return sorted.len() + 1;
+    }
+
+    fn find_binary(sorted: &[i32], target: i32) -> usize {
+        let mut left = 0;
+        let mut right = sorted.len();
+
+        while left < right {
+            let mid = left + (right - left) / 2;
+
+            if sorted[mid] <= target {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left + 1;
+    }
+
+    fn find_simd(sorted: &[i32], target: i32) -> usize {
+        let sorted_len = sorted.len();
+
+        let target_8: Simd<i32, 8> = Simd::splat(target);
+
+        let mut i: usize = 0;
+
+        while i < sorted_len {
+            if sorted_len - i < 8 {
+                // this is the last batch: iterate
+                while i < sorted_len {
+                    if target < sorted[i] {
+                        return i + 1;
+                    }
+                    i += 1;
+                }
+            } else {
+                // simd
+                let next_8: Simd<i32, 8> = Simd::from_slice(&sorted[i..(i + 8)]);
+
+                let matches = target_8.simd_lt(next_8);
+                if matches.any() {
+                    return i + (matches.to_bitmask().trailing_zeros() as usize) + 1;
+                }
+
+                i += 8;
+            }
+        }
+
+        return sorted_len + 1;
+    }
 
     use test::black_box;
     use test::Bencher;
